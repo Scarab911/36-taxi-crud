a1b2327c07694a71d9e5c1e1227fc808a871cffd
Paulius B <Paulspremium@gmail.com>
2021-07-23 18:48:41 +0300

    intro

---
+++ Taxi.js
@@ -0,0 +1,34 @@
+class Taxi {
+    constructor() {
+
+    }
+
+    intro() {
+
+    }
+
+    updatePrice() {
+
+    }
+
+    ride() {
+
+    }
+
+    stats() {
+
+    }
+
+    ride() {
+
+    }
+
+    stats() {
+
+    }
+
+    updatePrice() {
+
+    }
+}
+module.exports = Taxi;
